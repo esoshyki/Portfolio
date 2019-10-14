ce9c34c26d8484cb89d4c6756f5a5d15c15a06c3
esoshyki <esoshyki@yandex.by>
2019-10-14 15:23:51 +0300

    fix: fix problems with slider on mobile version

--- src/js/mobile_show_info.js
+++ src/js/mobile_show_info.js
@@ -1,33 +1,42 @@
 const mobile_show_info = () => {
     const button = document.querySelector('._show_information');
     const content = document.querySelector('._content')
-    const information = document.querySelector('._information');
+    const information = document.querySelectorAll('._information');
+    const body = document.querySelector('body');
     let hidden = true
 
     const show_info = () => {
-        console.log(content.offsetWidth)
         if (hidden) {
-            information.style.display = 'flex';
-            information.style.flexDirection = 'column';
+            information.forEach(el => {
+                el.style.display = 'flex'
+                el.style.flexDirection = 'column';
+            })
             button.style.transform = 'rotate(180deg)'
             hidden = false;
         }
         else {
-            information.style.display = 'none';
+            information.forEach(el => {
+                el.style.display = 'none'
+            })
             button.style.transform = 'rotate(0deg)'     
             hidden = true       
         }
     }
 
     const resize = () => {
-        console.log(content.offsetWidth)
-        if (content.offsetWidth > 600 ) {
-            information.style.display = 'block';           
+        const width = body.offsetWidth
+        information.forEach(el => {
+            el.style.display =  width > 600 ? 'block' : 'none'
+        })
+        if (width <= 600) {
+            button.style.transform = 'rotate(0)';
+            hidden = true
         }
     }
 
+    window.addEventListener('resize', resize)
     button.addEventListener('click', show_info);
-    window.addEventListener('resize', resize);
+
 }
 
 export default mobile_show_info
--- src/js/projects_slide.js
+++ src/js/projects_slide.js
@@ -1,21 +1,82 @@
+import mobile_show_info from './mobile_show_info'
+
 const project_slide = () => {
     const theallow = document.querySelector('.theyallow');
     const repair = document.querySelector('.repair-design');
+    const theallow_img = document.querySelector('._image-link');
+    const repair_img = document.querySelector('._image-link');
+
     let theallow_active = true
+    let dx = 0
     const changeContent = () => {
+        console.log('change start')
         if (theallow_active) {
             theallow.style.display = 'none';
-            repair.style.display = 'grid';
+            repair.style.display = 'flex';
             theallow_active = false
         }
         else {
-            theallow.style.display = 'grid';
+            theallow.style.display = 'flex';
             repair.style.display = 'none';
             theallow_active = true
         }
     }
+
+    const changeContentMobile = () => {
+        if (theallow_active) {
+            theallow.style.display = 'none';
+            repair.style.display = 'flex';
+            dx = 0
+        }
+        else {
+            theallow.style.display = 'flex';
+            repair.style.display = 'none';
+            dx = 0
+        }
+    }
+
     document.querySelector('._left_slider').addEventListener('click', changeContent);
     document.querySelector('._right_slider').addEventListener('click', changeContent);    
+
+    const swipe = (event) => {
+        const startX = event.touches[0].clientX
+        let el;
+        if (theallow_active) {
+            el = theallow
+        }
+
+        const move = (e) => {
+            dx = startX - e.touches[0].clientX
+            console.log(`dx = ${dx}`)
+            el.style.marginLeft = `${-dx}px`;
+            if (dx > 100 ||dx < -100) {
+                changeContentMobile()
+                el.style.marginLeft = 0
+                dx = 0  
+                if (theallow_active === true) {
+                    theallow_active = false;
+                    console.log('here')
+                    repair_img.addEventListener('touchstart', swipe, true);
+                    theallow_img.removeEventListener('touchstart', swipe)
+                    theallow_img.removeEventListener('touchmove', move)
+                }
+                else {
+                    theallow_active = true
+                    theallow_img.addEventListener('touchstart', swipe, true);
+                    repair_img.removeEventListener('touchstart', swipe)
+                    repair_img.removeEventListener('touchmove', move)
+                }
+            }
+        }
+
+        theallow_active ? theallow_img.addEventListener('touchmove', move) : repair_img.addEventListener('touchmove', move)
+
+    }
+
+
+    theallow_img.addEventListener('touchstart', swipe, true);
+    
+
 }
 
 export default project_slide
